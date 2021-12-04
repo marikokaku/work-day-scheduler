d4256a6f95013855525ab25615699b0c1021a9af
Mariko <mkaku98@gmail.com>
2021-12-03 22:14:15 -0800

    hour colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,21 @@ var saveBtn = $(".saveBtn");
 
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
+function hourblockColor () {
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currHour = parseInt($(this).attr("id"));
+
+        if (currHour > hour) {
+            $(this).addClass("future");
+        } else if (currHour < hour) {
+            $(this).addClass("past");
+        } else {
+            $(this).addClass("present");
+        }
+
+    })
+};
+
+hourblockColor();
